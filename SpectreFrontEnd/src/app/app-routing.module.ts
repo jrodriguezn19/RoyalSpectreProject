ef50a4eb60b4e07d7e1bbd070e78d3db1c273526
GiovanniTjahjono <ivangiovn@gmail.com>
2019-08-21 10:05:35 +1000

    Adding files

--- SpectreFrontEnd/src/app/app-routing.module.ts
+++ SpectreFrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { HomeComponent } from './home/home.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: MainMenuComponent},
+  { path: 'project-detail', component: ProjectDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- SpectreFrontEnd/src/app/app.module.ts
+++ SpectreFrontEnd/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideMenuComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    ProjectDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +24,6 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
