6b3d14eea4b8f3f3eb58b1f83a8fc49f26feee78
Pilling <jaydnp@gmail.com>
2019-08-25 16:53:11 +1000

    add HTTP request code

--- SpectreBackEnd/src/server.js
+++ SpectreBackEnd/src/server.js
@@ -17,6 +17,21 @@ app.use(bodyParser.json());
 // Routes
 app.use('/', require('../routes/api'));
 
+//HTTP Requests
+app.get('/', (req, res) => {
+    return res.send('Recieved a GET HTTP method');
+});
+app.post('/', (req, res) => {
+    return res.send('Recieved a POST HTTP method');
+});
+app.put('/', (req, res) => {
+    return res.send('Recieved a PUT HTTP method');
+});
+app.delete('/', (req, res) => {
+    return res.send('Recieved a DELETE HTTP method');
+});
+
+
 // Start Server
 app.listen(process.env.PORT, () => {
     console.log('Example app listening on port' , process.env.PORT)
