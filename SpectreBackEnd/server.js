a97a880182db76238ec244e7e2cf499ffd263a92
Pilling <jaydnp@gmail.com>
2019-09-01 12:49:11 +1000

    Merge server.js file

--- SpectreBackEnd/server.js
+++ SpectreBackEnd/server.js
@@ -1,26 +1,5 @@
 // Dependencies
 const express = require('express');
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-// MongoDB
-mongoose.connect('mongodb://localhost/rest_test');
-
-// Express
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-
-// Routes
-app.use('/', require('./routes/api'));
-
-// Start Server
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
-    });
-=======
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -101,4 +80,3 @@ startDatabase().then(async () => {
 app.listen(8000, () => {
    console.log('Example app listening on port 8000!')
 });
->>>>>>> BackEnd
