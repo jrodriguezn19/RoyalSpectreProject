f82c1dc65f7cbf0525491b2d6250f7c56597c865
GiovanniTjahjono <ivangiovn@gmail.com>
2019-09-03 21:30:09 +1000

    Adding new structure file and login

--- SpectreFrontEnd/src/app/app.module.ts
+++ SpectreFrontEnd/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,11 +23,13 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     ProfileComponent,
     TopMenuComponent,
-    LoginComponent
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
