fb40524a70f14a974a8ff71c1fe16ea1d1afde74
yogeshadhikari <yogeshmike@gmail.com>
2019-09-14 01:26:01 +1000

    installing AOS

--- SpectreFrontEnd/src/app/app.component.ts
+++ SpectreFrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Spectre';
-  text = 5 + 5;
+  ngOnInit(){
+    AOS.init();
+  }
 }
