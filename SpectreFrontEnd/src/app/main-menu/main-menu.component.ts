9a3fce56244146def3d040bd4ed53759b6a8a052
yogeshadhikari <yogeshmike@gmail.com>
2019-09-18 16:19:28 +1000

    Scroll Animation on Home Page

--- SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
+++ SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-main-menu',
@@ -9,7 +10,12 @@ export class MainMenuComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit(){
+    AOS.init({
+      offset: 200, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 1000 // values from 0 to 3000, with step 50ms
+    });
+   }
 
 }
