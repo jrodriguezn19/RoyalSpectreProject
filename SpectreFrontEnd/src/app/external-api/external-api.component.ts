6cb8e7e08aa1e532ecdca41b18f384c6ac5980e9
jrodriguez19 <jrodrigueznocua@gmail.com>
2019-09-23 13:51:47 +1000

    upload card to Firebase placed in Post menu

--- SpectreFrontEnd/src/app/external-api/external-api.component.ts
+++ SpectreFrontEnd/src/app/external-api/external-api.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
-
+import  {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-external-api',
@@ -10,7 +10,8 @@ import { ApiService } from '../api.service';
 export class ExternalApiComponent implements OnInit {
   responseJson: string;
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService, private http: HttpClient) { }
+
 
   ngOnInit() {
   }
@@ -19,6 +20,24 @@ export class ExternalApiComponent implements OnInit {
     this.api.ping$().subscribe(
       res => this.responseJson = res
     );
+  }
+
+  selectedFile : File = null;
+
+  onFileSelected(event){
+    console.log(event);
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload(){
+    console.log("uploading");
+    const fd = new FormData();
+    fd.append('Testing', this.selectedFile, this.selectedFile.name);
+    this.http.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd ).subscribe( res =>{
+       console.log("Uploaded finished"); 
+       console.log(res);
+    })
+
   }
 
 }
--- SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
+++ SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
-// Added by George
-import  {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-main-menu',
@@ -10,7 +8,6 @@ import  {HttpClient} from '@angular/common/http';
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
 
   ngOnInit(){
     AOS.init({
@@ -19,23 +16,4 @@ export class MainMenuComponent implements OnInit {
       duration: 1000 // values from 0 to 3000, with step 50ms
     });
    }
-
-   selectedFile : File = null;
-
-   onFileSelected(event){
-     console.log(event);
-     this.selectedFile = <File>event.target.files[0];
-   }
-
-   onUpload(){
-     console.log("uploading");
-     const fd = new FormData();
-     fd.append('Testing', this.selectedFile, this.selectedFile.name);
-     this.http.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd ).subscribe( res =>{
-        console.log("Uploaded finished"); 
-        console.log(res);
-     })
-
-   }
-
 }
