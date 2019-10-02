4297d096b1fae9b1c47189087316e90549c66234
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-02 22:17:18 +1000

    Post Project 90% Finish

---
+++ SpectreBackEnd/model/comments.js
@@ -0,0 +1,8 @@
+const mongoose= require('mongoose');
+
+const imagesSchema = mongoose.Schema({
+    id_user: { type: String, required: true },
+    id_image: { type: String, required: true }
+});
+
+module.exports = mongoose.model('images', imagesSchema);
