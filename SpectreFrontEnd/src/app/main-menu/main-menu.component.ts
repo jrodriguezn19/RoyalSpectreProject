3b86dc52f8cdaf7fc8fe759d42e47cb693e0cf5d
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-06 22:56:03 +1100

    Refining Profile Design and Main Menu Function

--- SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
+++ SpectreFrontEnd/src/app/main-menu/main-menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+import { Project } from '../project.model';
+import Axios from 'axios';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-main-menu',
@@ -7,13 +10,20 @@ import * as AOS from 'aos';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
-
-
+  projects: Project[] = [];
+  projectsUpdated =new Subject<Project[]>();
   ngOnInit(){
     AOS.init({
       offset: 200, // offset (in px) from the original trigger point
       delay: 0, // values from 0 to 3000, with step 50ms
       duration: 1000 // values from 0 to 3000, with step 50ms
     });
+    Axios.get<{message: string, projects: Project[]}>('http://localhost:8080/project')
+    .then((projectData) => {
+      this.projects = projectData.data.projects;
+      this.projectsUpdated.next([...this.projects]);
+      console.log(this.projects);
+    });
+
    }
 }
--- SpectreFrontEnd/src/app/profile/profile.component.ts
+++ SpectreFrontEnd/src/app/profile/profile.component.ts
@@ -12,14 +12,15 @@ export class ProfileComponent implements OnInit {
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
+    
   }
 
-  enteredProjectName = '';
-  enteredSummary = '';
-  imageUpload = '';
-  id_user = '';
-  target_fund = '';
-  status = '';
+  enteredProjectName: String = '';
+  enteredSummary: String = '';
+  imageUpload: String = '';
+  id_user: String;
+  target_fund: String = '';
+  status: String;
 
   fileData: File = null;
   fileProgress(fileInput: any) {
@@ -32,43 +33,38 @@ export class ProfileComponent implements OnInit {
     this.selectedFile = <File>event.target.files[0];
   }
 
-
-
-  newProject() {
-    axios.post('http://localhost:8000/newProject', { projectName: this.enteredProjectName, projectSummary: this.enteredSummary, image: this.imageUpload })
-      .then(function (response) {
-        console.log(response.data['message']);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  CreateProject() {
+  CreateProject(id_user: String) {
     
-    let fileName = 'Hi Ivan.jpg';
-    let url = 'https://firebasestorage.googleapis.com/v0/b/royalspectreproject.appspot.com/o/' + fileName + '?alt=media&token=57f0fd2a-30f4-469c-8371-d30e66a47975';
+    let fileName = 'uio';
+    let ext = this.selectedFile.name.split('.')[1];
     
+    alert(ext);
     const fd = new FormData();
-
-    fd.append('Testing', this.selectedFile, fileName);
-    axios.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd).then(res => {
-      console.log(res);
-      alert("Post Complete");
-    })
+    let that = this;
     
-    axios.post('http://localhost:8000/createProject', { id_user: this.id_user, target_fund: this.target_fund, status: this.status, image_url: url })
+    axios.post('http://localhost:8080/imageName')
       .then(function (response) {
-        console.log(response.data['message']);
+        alert('Response : ' + response.data);
+        fileName = response.data + '.' + ext;
+        let url = 'https://firebasestorage.googleapis.com/v0/b/royalspectreproject.appspot.com/o/' + fileName + '?alt=media&token=57f0fd2a-30f4-469c-8371-d30e66a47975';
+        fd.append('Testing', that.selectedFile, fileName);
+        axios.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd).then(res => {
+          console.log(res);
+          alert("Post Complete");
+        })
+        axios.post('http://localhost:8080/createProject', { id_user: id_user, target_fund: that.target_fund, status: that.status, image_url: url })
+          .then(function (response) {
+            console.log(response.data['message']);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       })
       .catch(function (error) {
         console.log(error);
       });
 
-    
-  
-
-
+      
   }
 
 }
