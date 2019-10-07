02aaf05bf036b6e2b6fd326ecbb91f5f7e04ce84
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-07 21:48:02 +1100

    Adding animation when creating project

--- SpectreFrontEnd/src/app/profile/profile.component.ts
+++ SpectreFrontEnd/src/app/profile/profile.component.ts
@@ -55,19 +55,17 @@ export class ProfileComponent implements OnInit {
     let fileName = 'uio';
     let ext = this.selectedFile.name.split('.')[1];
     
-    alert(ext);
     const fd = new FormData();
     let that = this;
     
     axios.post('http://localhost:8000/imageName')
       .then(function (response) {
-        alert('Response : ' + response.data);
         fileName = response.data + '.' + ext;
         let url = 'https://firebasestorage.googleapis.com/v0/b/royalspectreproject.appspot.com/o/' + fileName + '?alt=media&token=57f0fd2a-30f4-469c-8371-d30e66a47975';
         fd.append('Testing', that.selectedFile, fileName);
         axios.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd).then(res => {
           console.log(res);
-          alert("Post Complete");
+          location.reload();
         })
         axios.post('http://localhost:8000/createProject', { user_name: user_name, id_user: id_user, target_fund: that.target_fund, status: that.status, image_url: url })
           .then(function (response) {
