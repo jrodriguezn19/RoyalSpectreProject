67db6caabe4045ebbf75471a42e0471e241f7594
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-07 21:47:41 +1100

    Adding animation when sending comment

--- SpectreFrontEnd/src/app/project-detail/project-detail.component.ts
+++ SpectreFrontEnd/src/app/project-detail/project-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 import axios from 'axios';
 import { Project } from '../project.model';
+import { Comment } from '../comment.model';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
@@ -10,20 +13,87 @@ import { Project } from '../project.model';
 export class ProjectDetailComponent implements OnInit {
   id: any;
   paramsSub: any;
-  project: Project = null;
-  constructor(private activatedRoute: ActivatedRoute) {
+  project: Project;
+
+
+  constructor(private activatedRoute: ActivatedRoute, public auth: AuthService) {
 
   }
-
+  projects: Project[] = [];
+  comments: Comment[] = [];
+  commentsUpdated = new Subject<Comment[]>();
+  projectsUpdated = new Subject<Project[]>();
   ngOnInit() {
     let that = this;
     this.paramsSub = this.activatedRoute.params.subscribe(params => this.id = params['id']);
-    axios.post('http://localhost:8000/selectedProject', { id: this.id })
+
+    axios.post('http://localhost:8000/selectedProject', { id_project: this.id })
       .then(function (response) {
         console.log(response.data['message']);
         //this.project = response.data.project;
         that.project = response.data.project;
-        
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    this.getComment();
+    
+
+      
+
+  }
+  //sendComment
+  getComment(){
+    axios.post<{ message: string, comments: Comment[] }>('http://localhost:8000/retrieveComments', { id_project: this.id })
+      .then((commentData) => {
+        console.log(commentData.data['message']);
+        this.comments = commentData.data.comments;
+        this.commentsUpdated.next([...this.comments]);
+        console.log(this.comments);
+      });
+  }
+  imageUpload: String = '';
+
+
+  fileData: File = null;
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+  }
+  selectedFile: File = null;
+
+  onFileSelected(event) {
+    console.log(event);
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  SendComment(id_user: String, user_name: String, profile_picture: String) {
+
+
+    //alert(Object.values(this.project)[0]);
+    console.log(this.project[0]._id);
+
+    let fileName = 'file';
+    let ext = this.selectedFile.name.split('.')[1];
+
+    const fd = new FormData();
+    let that = this;
+    axios.post('http://localhost:8000/imageName')
+      .then(function (response) {
+        fileName = response.data + '.' + ext;
+        let url = 'https://firebasestorage.googleapis.com/v0/b/royalspectreproject.appspot.com/o/' + fileName + '?alt=media&token=57f0fd2a-30f4-469c-8371-d30e66a47975';
+        fd.append('Testing', that.selectedFile, fileName);
+        axios.post('https://us-central1-royalspectreproject.cloudfunctions.net/uploadFile', fd).then(res => {
+          console.log(res);
+          location.reload();
+        })
+        axios.post('http://localhost:8000/sendComment', { id_user: id_user, id_project: that.project[0]._id, user_name: user_name, image_url: url, profile_picture: profile_picture })
+          .then(function (response) {
+            console.log(response.data['message']);
+            
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       })
       .catch(function (error) {
         console.log(error);
@@ -31,5 +101,4 @@ export class ProjectDetailComponent implements OnInit {
 
 
   }
-
 }
