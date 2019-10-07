a32d9343ef28f45db834113937d3d12d6f2ef2a9
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-07 15:23:20 +1100

    Refinement structure comments model and projects model

--- SpectreBackEnd/model/comments.js
+++ SpectreBackEnd/model/comments.js
@@ -2,7 +2,11 @@ const mongoose= require('mongoose');
 
 const commentsSchema = mongoose.Schema({
     id_user: { type: String, required: true },
-    id_image: { type: String, required: true }
+    id_project: { type: mongoose.Schema.Types.ObjectId, required: true },
+    image_name: { type: String, required: true },
+    user_name: { type: String, required: true },
+    image_url: { type: String, required: true },
+    date: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('comments', commentsSchema);
--- SpectreBackEnd/model/projects.js
+++ SpectreBackEnd/model/projects.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose');
 
 const projectsSchema = mongoose.Schema({
     id_user: { type: String, required: true },
+    user_name: { type: String, required: true },
     image_url: { type: String, required: true },
     target_fund: { type: Number, required: true },
     current_fund: { type: Number, required: true, default: 0 },
