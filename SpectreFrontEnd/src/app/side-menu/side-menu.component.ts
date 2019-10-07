6f6f32f04f5a038b435a20e43d8541112e47d2ef
GiovanniTjahjono <ivangiovn@gmail.com>
2019-10-08 00:30:21 +1100

    Most popular now work on side bar

--- SpectreFrontEnd/src/app/side-menu/side-menu.component.ts
+++ SpectreFrontEnd/src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { Subject } from 'rxjs';
+import Axios from 'axios';
 
 @Component({
   selector: 'app-side-menu',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-menu.component.css']
 })
 export class SideMenuComponent implements OnInit {
-
+  projects: Project[] = [];
+  projectsUpdated =new Subject<Project[]>();
+  sortBy : String = "newest";
   constructor() { }
 
   ngOnInit() {
+    localStorage.setItem('sortBy', 'popular');
+    Axios.get<{message: string, projects: Project[]}>('http://localhost:8000/projectPopularSideMenu')
+    .then((projectData) => {
+      this.projects = projectData.data.projects;
+      this.projectsUpdated.next([...this.projects]);
+      console.log(this.projects);
+      
+    });
   }
 
 }
