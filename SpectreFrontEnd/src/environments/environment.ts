06e743ec2f23e6eb29a0af419b8a4ce8ef433394
jrodriguez19 <jrodrigueznocua@gmail.com>
2019-10-10 16:28:17 +1100

    Comments added, removed unused functions folder and External api component

--- SpectreFrontEnd/src/environments/environment.ts
+++ SpectreFrontEnd/src/environments/environment.ts
@@ -1,7 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+/**
+ * Configuration required to make use of Firebase API
+ */
 export const environment = {
   production: false,
   firebase: {
@@ -14,12 +13,3 @@ export const environment = {
     appId: '1:796518612029:web:be9f21c43c2d70cf3f3e54'
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
